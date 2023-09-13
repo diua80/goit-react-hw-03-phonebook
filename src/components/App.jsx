b90ea4dea3d5f6c6f8ac5be4b6c2b193e5f46bca
Iryna Yatsenko <iryna.didova@gmail.com>
2023-09-13 22:27:14 +0300

    clone hw 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import { Filter } from 'components/Filters/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  handleFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  addContact = (contactName, number) => {
+    const newContact = {
+      id: nanoid(),
+      name: contactName,
+      number: number,
+    };
+    const nameExists = this.state.contacts.some(
+      contact => contact.name.toLowerCase() === contactName.toLowerCase()
+    );
+    if (nameExists) {
+      alert(`${contactName} вже присутнє у списку контактів!`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+  
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-};
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter
+          handleFilterChange={this.handleFilter}
+          filter={this.state.filter}
+        />
+        <ContactList
+          contacts={filteredContacts}
+          handleDeleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
